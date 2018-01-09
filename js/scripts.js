d6a2b26d3b920fb8ea3e406b5d799b67ab8b2c77
Jared Clemmensen and Maggie Summers <jared@>
2018-01-09 13:24:28 -0800

    add content for first two rows.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,48 @@
 $(document).ready(function() {
 
+// first row
 $("#card1").click(function() {
   $(".front1").toggle();
-  $(".back1").toggle();
+  $("#back1").toggle();
 });
 
 $("#card2").click(function() {
   $(".front2").toggle();
-  $(".back2").toggle();
+  $("#back2").toggle();
 });
+
+$("#card3").click(function() {
+  $(".front3").toggle();
+  $("#back3").toggle();
+});
+
+$("#card4").click(function() {
+  $(".front4").toggle();
+  $("#back4").toggle();
+});
+
+// second row
+$("#card5").click(function() {
+  $(".front5").toggle();
+  $("#back5").toggle();
+});
+
+$("#card6").click(function() {
+  $(".front6").toggle();
+  $("#back6").toggle();
+});
+
+$("#card7").click(function() {
+  $(".front7").toggle();
+  $("#back7").toggle();
+});
+
+$("#card8").click(function() {
+  $(".front8").toggle();
+  $("#back8").toggle();
+});
+
+// third row
 
 
 });
