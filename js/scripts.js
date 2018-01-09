835863ad2a24daaffc49f66b50883effe3d3004d
Jared Clemmensen and Maggie Summers <jared@>
2018-01-09 11:43:57 -0800

    add flashcard functionality

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+$("#card1").click(function() {
+  $(".front1").toggle();
+  $(".back1").toggle();
+});
+
+$("#card2").click(function() {
+  $(".front2").toggle();
+  $(".back2").toggle();
+});
+
+
+});
